014a7df058b7c24e3549581d54455a69552383d4
José Solís <j.solisgom@gmail.com>
2024-01-03 21:22:37 -0300

    Se agrega relacion con modelo Category

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'description', 
+        'price', 
+        'is_enabled',
+        'category_id'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
